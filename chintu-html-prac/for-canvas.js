5ee39937fd1a7208f1aa923d1220d775fc68cc38
saiksk939 <ranto93900@gmail.com>
2022-07-09 15:08:26 +0530

    changes

--- chintu-html-prac/for-canvas.js
+++ chintu-html-prac/for-canvas.js
@@ -1,12 +1,11 @@
 function logo(){
-    var corn=document.getElementById("mycanvas");
-    var ctxt=corn.getContext("2d");
+    var ranto=document.getElementById("mycanvas");
+    var ctxt=ranto.getContext("2d");
 
     ctxt.font="50px elephant";
     ctxt.fillStyle="red";
-    ctxt.fillText("SAIKSK",150,200);
+    ctxt.fillText("SAIKSK",150,200);  /* "saiksk", to move left & right, to move up & down */
     ctxt.strokeStyle="yellow";
-    ctxt.strokeText("SAIKSK",150,200);
-
+    ctxt.strokeText("SAIKSK",150,200);   /* "saiksk", to move left & right, to move up & down */
 }
 window.addEventListener("load",logo,false);
