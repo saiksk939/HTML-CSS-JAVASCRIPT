74189daa1b8da529014b414cd3fc76f8a9d45505
saiksk939 <ranto93900@gmail.com>
2022-07-13 14:38:40 +0530

    changes

--- chintu-html-prac/for-canvas8.js
+++ chintu-html-prac/for-canvas8.js
@@ -4,18 +4,18 @@ function logo(){
 
     ctxt.fillStyle="red";
     ctxt.fillRect(10,10,25,15);
-    ctxt.scale(1.5,1.5);
+    ctxt.scale(3,3);
     ctxt.fillStyle="blue";
     ctxt.fillRect(10,10,25,15);
-    ctxt.scale(1.5,1.5);
+    ctxt.scale(3,3);
     ctxt.fillStyle="green";
     ctxt.fillRect(10,10,25,15);
-    ctxt.scale(1.5,1.5);
+    ctxt.scale(3,3);
     ctxt.fillStyle="aqua";
     ctxt.fillRect(10,10,25,15);
-    ctxt.scale(1.5,1.5);
+    ctxt.scale(3,3);
     ctxt.fillStyle="orange";
     ctxt.fillRect(10,10,25,15);
-    ctxt.scale(1.5,1.5);
+    ctxt.scale(3,3);
 }    
 window.addEventListener("load", logo, false);
